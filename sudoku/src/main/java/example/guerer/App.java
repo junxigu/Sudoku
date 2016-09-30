d9b4d5f7500d56adc43ec9ed878638f5ca44d4ac
junxigu <junxigu@cn.ibm.com>
2016-09-30 10:36:16 +0800

    first init

---
+++ sudoku/src/main/java/example/guerer/App.java
@@ -0,0 +1,13 @@
+package example.guerer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
